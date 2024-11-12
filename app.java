d30bb36768a766266ad0ecde2399ec8c0f5b7d15
Tharun-Salgar <148680146+Tharun-Salgar@users.noreply.github.com>
2024-11-12 12:37:07 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
-    // Main method - the entry point of the Java application
+
     public static void main(String[] args) {
-        // Print a message to the console
+        
         System.out.println("Hello, World!");
     }
 }
